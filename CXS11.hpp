bbe7eaf946ff172cfd8d4fcc9ef021407f13f59b
Cups Studio <mr.man8man@gmail.com>
2026-02-15 20:27:46 +0000

    Add files via upload

---
+++ CXS11.hpp
@@ -0,0 +1,137 @@
+#ifndef CXS11_HPP
+#define CXS11_HPP
+
+#include <iostream>
+#include "Cpt_t.hpp"
+using namespace std;
+
+namespace cp{
+    template<typename T>
+    void print(T value) {
+        cout << value;
+    }
+
+    template<typename T>
+    void println(T value) {
+        cout <<value <<"\n";
+    }
+
+    template<typename T>
+    void printc(T value) {
+        cout <<value <<"\n";
+    }
+
+    template<typename T>
+    void printcln(T value) {
+        cout <<value <<"\n";
+    }
+
+    template<typename T>
+    void out(T value) {
+        cout <<value <<"\n";
+    }
+
+    template<typename T>
+    void outln(T value) {
+        cout <<value <<"\n";
+    }
+
+    template<typename T>
+    void flprint(T value) {
+        cout << value << flush;
+    }
+
+    template<typename T>
+    void flprintln(T value) {
+        cout << value <<"\n" << flush ;
+    }
+
+    template<typename T>
+    void flout(T value) {
+        cout << value << flush;
+    }
+
+    template<typename T>
+    void floutln(T value) {
+        cout << value <<"\n" << flush ;
+    }
+
+    template<typename T>
+    void flprintc(T value) {
+        cout << value << flush;
+    }
+
+    template<typename T>
+    void flprintcln(T value) {
+        cout << value <<"\n" << flush ;
+    }
+
+
+
+    template<typename T>
+    void input(T& value) {
+        cin >> value;
+    }
+
+    template<typename T>
+    void inputc(T& value) {
+        cin >> value;
+    }
+
+    template<typename T>
+    void put(T& value) {
+        cin >> value;
+    }
+
+
+    template<typename T1, typename T2>
+    void add(T1 a, T2 b) {
+        cout << a + b << "\n";
+    }
+
+    template<typename T1, typename T2>
+    void sub(T1 a, T2 b) {
+        cout << a - b << "\n";
+    }
+
+    template<typename T1, typename T2>
+    void multi(T1 a, T2 b) {
+        cout << a * b << "\n";
+    }
+
+    template<typename T1, typename T2>
+    void div(T1 a, T2 b) {
+        if (b == 0) {
+            cout << "CXS-Error: Can't divide by zero" << endl;
+        } else {
+            double result = static_cast<double>(a) / b;
+            cout << result << endl;
+        }
+    }
+
+    /*Cxs is a High level object oriented programing language
+    * made by Cups Studio
+    * The original creactor was ClippyCraft
+    * and the helper was gpt
+    */
+
+}
+
+
+#define ct std
+#define es else
+#define neither else
+#define esif else if
+#define either else if
+#define swch switch
+#define when while
+#define for_this for
+#define fr for
+#define rehave return
+
+#define sr srand
+#define tm time
+
+
+
+#endif //CXS11_HPP
---
+++ Cpt_t.hpp
@@ -0,0 +1,48 @@
+#ifndef CPT_T_HPP//CPT U8 (CPT V2.0)
+#define CPT_T_HPP
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+using nums = int;
+using str = std::string;
+using dob = double;
+using bl = bool;
+using fl = float;
+using cr = char;
+using lg = long;
+
+using let_nums = int;
+using let_str = std::string;
+using let_dob = double;
+using let_bl = bool;
+using let_fl = float;
+using let_cr = char;
+using let_lg = long;
+using let_db = double;
+using let_vd = void;
+using let_nort = void;
+#define var auto //from kotlin,js
+#define dyn auto // from CXS (CXS own syntax)
+#define letd auto // from CXS (CXS own syntax)
+#define funk auto // from CXS (CXS own syntax)
+#define val const //from kotlin
+#define immut const // from CXS (CXS own syntax)
+#define letc const // from CXS (CXS own syntax)
+#define fn auto // from rust
+#define fun auto //from kotlin,go etc
+#define let auto //from js
+#define func auto // from CXS (CXS own syntax)
+#define function auto //from js
+
+using funv = void;
+using funnm = int;
+using funst = std::string;
+using fund = double;
+using funf = float;
+using funb = bool;
+using funcr = char;
+using funlg = long;
+
+#endif //CPT_T_HPP
