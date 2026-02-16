7a61313f548345be2927f97a243805ba21bfa949
Cups Studio <mr.man8man@gmail.com>
2026-02-16 18:08:20 +0000

    Refactor Cpt_t.hpp to include type aliases and macros
    Added new type aliases and macros for various data types from Kotlin and CXS syntax.

--- Cpt_t.hpp
+++ Cpt_t.hpp
@@ -13,39 +13,45 @@ using namespace short_types{
   using fl = float;
   using cr = char;
   using lg = long;
+  #define var auto //from kotlin,js
+  #define val const //from kotlin
 }
 
+using namespace let_types{
+  using let_nums = int;
+  using let_str = std::string;
+  using let_dob = double;
+  using let_bl = bool;
+  using let_fl = float;
+  using let_cr = char;
+  using let_lg = long;
+  using let_db = double;
+  using let_vd = void;
+  using let_nort = void;
+}
+using namespace CXS_types{
+  #define dyn auto // from CXS (CXS own syntax)
+  #define immut const // from CXS (CXS own syntax)
+  #define letc const // from CXS (CXS own syntax)
+  #define letd auto // from CXS (CXS own syntax)
+  #define funk auto // from CXS (CXS own syntax)
+}
 
-using let_nums = int;
-using let_str = std::string;
-using let_dob = double;
-using let_bl = bool;
-using let_fl = float;
-using let_cr = char;
-using let_lg = long;
-using let_db = double;
-using let_vd = void;
-using let_nort = void;
-#define var auto //from kotlin,js
-#define dyn auto // from CXS (CXS own syntax)
-#define letd auto // from CXS (CXS own syntax)
-#define funk auto // from CXS (CXS own syntax)
-#define val const //from kotlin
-#define immut const // from CXS (CXS own syntax)
-#define letc const // from CXS (CXS own syntax)
-#define fn auto // from rust
-#define fun auto //from kotlin,go etc
-#define let auto //from js
-#define func auto // from CXS (CXS own syntax)
-#define function auto //from js
+using namespace functions{
+  #define fn auto // from rust
+  #define fun auto //from kotlin,go etc
+  #define let auto //from js
+  #define func auto // from CXS (CXS own syntax)
+  #define function auto //from js
+  using funv = void;
+  using funnm = int;
+  using funst = std::string;
+  using fund = double;
+  using funf = float;
+  using funb = bool;
+  using funcr = char;
+  using funlg = long;
+}
 
-using funv = void;
-using funnm = int;
-using funst = std::string;
-using fund = double;
-using funf = float;
-using funb = bool;
-using funcr = char;
-using funlg = long;
 
 #endif //CPT_T_HPP
