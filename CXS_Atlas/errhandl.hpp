76c6afcfcb1dc7e8a962aac017a7cb8e0c91e26b
Cups Studio <mr.man8man@gmail.com>
2026-03-24 12:56:16 +0100

    Add files via upload

---
+++ CXS_Atlas/errhandl.hpp
@@ -0,0 +1,7 @@
+#ifndef ERRHANDL_HPP
+#define ERRHANDL_HPP
+
+std::string diverr = "CXS-Error: Can't divide by zero";
+std::string typerr = "[CXS-ERROR]: Can't enter any type rather than int in arthemtics";
+
+#endif //ERRHANDL_HPP
