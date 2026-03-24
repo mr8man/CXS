953bd114ee86654256e09bca20d19c9964b46115
Cups Studio <mr.man8man@gmail.com>
2026-03-24 13:01:32 +0100

    Delete CXS_Atlas directory

--- CXS_Atlas/errhandl.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef ERRHANDL_HPP
-#define ERRHANDL_HPP
-
-std::string diverr = "CXS-Error: Can't divide by zero";
-std::string typerr = "[CXS-ERROR]: Can't enter any type rather than int in arthemtics";
-
-#endif //ERRHANDL_HPP
