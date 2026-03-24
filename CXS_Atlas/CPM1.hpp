4d2142d2829400e49b9fdf29b87d4a731fe9c6bb
Cups Studio <mr.man8man@gmail.com>
2026-03-24 15:40:57 +0100

    Update CPM1.hpp

--- CXS_Atlas/CPM1.hpp
+++ CXS_Atlas/CPM1.hpp
@@ -7,32 +7,36 @@
 using std::cout;
 
 namespace stdmath{
-    
-    template<typename T1, typename T2>
-    void add(T1 a, T2 b) {
-        cout << a + b << "\n";
+
+    template<typename... Args>
+    void add(Args... args) {
+        std::cout << ( ... + args ) << "\n";
     }
 
-    template<typename T1, typename T2>
-    void sub(T1 a, T2 b) {
-        cout << a - b << "\n";
+    template<typename... Args>
+    void sub(Args... args) {
+        std::cout << ( ... - args ) << "\n";
     }
 
-    template<typename T1, typename T2>
-    void multi(T1 a, T2 b) {
-            cout << a * b << "\n";
+    template<typename... Args>
+    void multi(Args... args) {
+        std::cout << ( ... * args ) << "\n";
     }
 
-    template<typename T1, typename T2>
-    void divs(T1 a, T2 b) {
-        if (b == 0) {
-            cout << diverr << "\n";
-        } else {
-            double result = static_cast<double>(a) / b;
-            cout << result;
+
+    template<typename T, typename... Args>
+    void divs(T first, Args... args) {
+        bool has_zero = ((args == 0) || ...);
+        if (has_zero) {
+            std::cout << "Error: division by zero\n";
+            return;
         }
+
+        double result = first;
+        ((result /= args), ...);
+        std::cout << result << "\n";
     }
-    
+
 }
 
 #endif //CPM+1_HPP
