60c55470f2ca98e30c777a4ae9eede00e3dc4183
Cups Studio <mr.man8man@gmail.com>
2026-03-24 15:43:52 +0100

    Add files via upload

---
+++ CXS_Atlas/example.cpp
@@ -0,0 +1,12 @@
+#include "CXSAtlas.hpp"
+#include "CPM1.hpp"
+
+using namespace stdio;
+using namespace stdmath;
+using namespace types;
+
+fun main() -> funnum{
+    println("Hello, World!");
+    add(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    return 0;
+}
