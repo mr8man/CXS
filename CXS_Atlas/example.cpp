ddb5b9e7d00c2093bfbf8c631beaa7f3424791ca
Cups Studio <mr.man8man@gmail.com>
2026-03-24 15:47:47 +0100

    Add user input and modify print statements

--- CXS_Atlas/example.cpp
+++ CXS_Atlas/example.cpp
@@ -6,7 +6,16 @@ using namespace stdmath;
 using namespace types;
 
 fun main() -> funnum{
-    println("Hello, World!");
+    str name;
+    num age;
+    var isAdmin;
+    flprintln("Hello, World!");
     add(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    sub(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    multi(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    divs(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+
+    input(name, age, isAdmin);
+    println("hello ", name , "is your age ", age, "are you an admin? ", isAdmin);
     return 0;
 }
