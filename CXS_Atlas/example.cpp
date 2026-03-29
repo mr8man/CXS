6da9fca2d496fa1d3ae07370b502735c5e910931
Cups Studio <mr.man8man@gmail.com>
2026-03-29 17:03:38 +0100

    Initialize isAdmin and remove from input
    Removed isAdmin from input parameters and initialized it as true.

--- CXS_Atlas/example.cpp
+++ CXS_Atlas/example.cpp
@@ -8,14 +8,14 @@ using namespace types;
 fun main() -> funnum{
     str name;
     num age;
-    var isAdmin;
+    var isAdmin = true;
     flprintln("Hello, World!");
     add(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
     sub(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
     multi(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
     divs(1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
 
-    input(name, age, isAdmin);
+    input(name, age);
     println("hello ", name , "is your age ", age, "are you an admin? ", isAdmin);
     return 0;
 }
