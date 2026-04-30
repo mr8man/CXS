ec90c9eb7efd9245a17cd1d1901cc071419b1fe0
Cups Studio <mr.man8man@gmail.com>
2026-04-30 16:23:14 +0200

    Add files via upload

---
+++ CXS5(CodeName CXS Atlas)/CXS_Atlas/errhandl.hpp
@@ -0,0 +1,11 @@
+#ifndef ERRHANDL_HPP
+#define ERRHANDL_HPP
+
+namespace errhandl{
+
+    std::string diverr = "[CPM-Error]: Can't divide by zero \n Please check your input and try again. \n";
+    std::string typerr = "[CPM-ERROR]: Can't enter any type rather than int in arthemtics \n Please check your input and try again. \n";
+
+}
+
+#endif //ERRHANDL_HPP
